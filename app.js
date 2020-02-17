b00e72afb19933a562b210c8c2eac8030aa4ccb9
Zane Schepke <zanecschepke@gmail.com>
2020-02-17 16:44:08 -0500

    Add user and index routes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
-const usersRouter = require('./routes/users');
 const reviewsRouter = require('./routes/reviews');
 
 const app = express();
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,55 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop Home' });
 });
 
+/* GET register page. */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+
+/* POST /register  */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+
+/* GET login page. */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login');
+});
+
+/* POST /login  */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login');
+});
+
+/* GET /profile  */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+
+/* PUT /profile/:user_id  */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile');
+});
+
+/* GET /forgot-password  */
+router.get('/forgot-pw', (req, res, next) => {
+  res.send('GET /forgot-pw');
+});
+
+/* PUT /forgot-password  */
+router.put('/forgot-pw', (req, res, next) => {
+  res.send('PUT /forgot-pw');
+});
+
+/* GET /reset-password  */
+router.get('/reset-pw/:token', (req, res, next) => {
+  res.send('GET /reset-pw');
+});
+
+/* PUT /reset-password  */
+router.put('/reset-pw/:token', (req, res, next) => {
+  res.send('PUT /reset-pw');
+});
+
+
 module.exports = router;
