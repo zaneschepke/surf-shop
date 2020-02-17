24fd85af2f0d965d25e70dff04ea6fa9711921d6
Zane Schepke <zanecschepke@gmail.com>
2020-02-17 15:11:15 -0500

    Added RESTful routes for posts

---
+++ routes/posts.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home posts index /posts */
+router.get('/', (req, res, next) => {
+    res.send('/posts');
+});
+
+/* GET get form for new post /posts/new */
+// New route before show route
+router.get('/new', (req, res, next) => {
+    res.send('/posts/new');
+});
+
+/* POST for create new post */
+router.post('/', (req, res, next) => {
+    res.send('CREATE /posts');
+});
+
+/* GET posts show /posts/:id */
+router.get('/:id', (req, res, next) => {
+    res.send(' show /posts/new');
+});
+
+/* GET posts edit for new post /posts/:id/edit */
+router.get('/:id/edit', (req, res, next) => {
+    res.send('EDIT /posts/:id/edit');
+});
+
+/* PUT posts update /posts/:id*/
+router.put('/:id', (req, res, next) => {
+    res.send('PUT /posts/:id');
+});
+
+/* DELETE posts remove /posts/:id*/
+router.delete('/:id', (req, res, next) => {
+    res.send('REMOVE /posts/:id');
+});
+
+module.exports = router;
